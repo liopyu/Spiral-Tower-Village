b1f13029859fb09f88a19438487c2e725b7fadcc
Liopyu <mistakensound@gmail.com>
2025-06-05 11:15:44 -0400

    first commit

---
+++ src/main/java/net/liopyu/lov/HobbitHillVillage.java
@@ -0,0 +1,11 @@
+package net.liopyu.lov;
+
+
+import net.minecraftforge.fml.common.Mod;
+
+@Mod("hobbit-hill-village")
+public class HobbitHillVillage {
+    public HobbitHillVillage() {
+    }
+
+}
