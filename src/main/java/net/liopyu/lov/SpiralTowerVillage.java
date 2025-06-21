a87be5de775d7bdbe6fa8b278e7778ca9604a0f9
Liopyu <mistakensound@gmail.com>
2025-06-21 08:44:51 -0400

    fixed 1.21+ loot tables

--- src/main/java/net/liopyu/lov/SpiralTowerVillage.java
+++ src/main/java/net/liopyu/lov/SpiralTowerVillage.java
@@ -1,7 +1,7 @@
 package net.liopyu.lov;
 import net.minecraftforge.fml.common.Mod;
 
-@Mod("spiral-tower-village")
+@Mod("spiral_tower_village")
 public class SpiralTowerVillage {
     public SpiralTowerVillage() {
     }
